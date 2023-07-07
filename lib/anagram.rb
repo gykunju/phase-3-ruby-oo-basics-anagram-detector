3b6b5941989e24499548bf045fc6ac9b2efa8a0d
Alvin <alvin.gikunju@student.moringaschool.com>
2023-07-07 19:25:31 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+    attr_reader :anagram
+    attr_accessor :word 
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram)
+        anagram.map do |word|
+            characters = word.chars
+            if characters.sort == @word.chars.sort 
+                @anagram = word
+            else
+                puts "#{word} is not an anagram of #{@word}"
+            end
+        end
+    end
+end
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
+puts listen.anagram
